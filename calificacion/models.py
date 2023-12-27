8d241b328758d270fa1f095909d7cdaec99823f4
AngelJadan <angel.jadan12@gmail.com>
2023-12-27 12:45:57 -0500

    modelado de objectos

--- calificacion/models.py
+++ calificacion/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from curso.models import MateriaCursoDocente
+from django.contrib.auth.models import User
 
 from persona.models import Estudiante
 
@@ -13,3 +15,141 @@ class MateriaEstudiante(models.Model):
         related_name="estudiante_materia",
         verbose_name="Estudiante",
     )
+    materia_curso = models.ForeignKey(
+        MateriaCursoDocente,
+        on_delete=models.CASCADE,
+        related_name="materia_curso",
+        verbose_name="Materia curso",
+    )
+
+    def __str__(self):
+        return f"{self.id}"
+
+    class Meta:
+        verbose_name = "Materia estudiante"
+        verbose_name_plural = "Materia estudiantes"
+
+
+CALIFICACION = (
+    ("0", "(NE) No evaluado"),
+    ("1", "(I) Destreza o aprendizaje iniciado"),
+    ("2", "(EP) Destreza o aprendizaje en proceso de desarrollo"),
+    ("3", "(A) Destreza o aprendizaje alcanzado"),
+)
+
+
+class CabeceraTrimestre(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Id")
+    numero_trimestre = models.IntegerField(verbose_name="Numero trimestre")
+    materia_estudiante = models.ForeignKey(
+        MateriaEstudiante,
+        on_delete=models.CASCADE,
+        related_name="materia_estudiante",
+        verbose_name="Materia estudiante",
+    )
+    aporte_cualitativo = models.CharField(
+        max_length=1, choices=CALIFICACION, verbose_name="Aporte cualitativo"
+    )
+    proyecto_integrador = models.FloatField(vebose_name="Proyecto integrador")
+    cualitativo_proyecto_integrador = models.CharField(
+        max_length=1,
+        choices=CALIFICACION,
+        verbose_name="Cualitativo proyecto integrador",
+    )
+    usuario = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Usuario",
+    )
+
+    def __str__(self):
+        return f"{self.id}"
+
+    class Meta:
+        verbose_name = "Cabecera trimestre"
+        verbose_name_plural = "Cabecera trimestres"
+
+
+TIPO_APORTE = (
+    ("1", "Actividades Disciplinares o indisciplinares individiales"),
+    ("2", "Actividades Disciplinares o indisciplinares grupales"),
+)
+
+
+class DetalleTrimestre(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Id")
+    tipo_aporte = models.CharField(
+        max_length=1, choices=TIPO_APORTE, verbose_name="Tipo aporte"
+    )
+    cabecera_trimestre = models.ForeignKey(
+        CabeceraTrimestre,
+        on_delete=models.CASCADE,
+        related_name="cabecera_trimestre",
+        verbose_name="Cabecera trimestre",
+    )
+
+    def __str__(self):
+        return f"{self.id}"
+
+    class Meta:
+        verbose_name = "Detalle trimestre"
+        verbose_name_plural = "Detalle trimestres"
+
+
+TIPO_ACTIVIDAD = (
+    ("1", "LECCIONES ORALES/ESCRITAS"),
+    ("2", "PRUEBAS BASE ESTRUCTURADA"),
+    ("3", "TAREAS/EJERCICIOS"),
+    ("4", "PROYECTOS INTEGRADORES"),
+    ("5", "EXPOSICIONES/FOROS"),
+    ("6", "TALLERES"),
+)
+
+
+class CabeceraActividad(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Id")
+    nombre = models.CharField(
+        max_length=1, choices=TIPO_ACTIVIDAD, verbose_name="Nombre"
+    )
+    detalle_trimestre = models.ForeignKey(
+        DetalleTrimestre,
+        on_delete=models.CASCADE,
+        related_name="detalle_trimestre",
+        verbose_name="Detalle trimestre",
+    )
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Usuario")
+
+    def __str__(self):
+        return f"{self.id}"
+
+    class Meta:
+        verbose_name = "Cabecera actividad"
+        verbose_name_plural = "Cabecera actividades"
+
+    def validar_actividad_tipo_aporte(self):
+        pass
+
+
+NOMBRE_TAREA = (
+    ("1", "LECCION ESCRITA DE LAS PARTES ORACION"),
+    ("2", "CUALITATIVO"),
+)
+
+
+class DetalleActividad(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Id")
+    nombre = models.CharField(max_length=1, choices=NOMBRE_TAREA, verbose_name="Nombre")
+    calificacion = models.IntegerField(verbose_name="Calificacion")
+    cabecera_actividad = models.ForeignKey(
+        CabeceraActividad,
+        on_delete=models.CASCADE,
+        related_name="cabecera_actividad",
+        verbose_name="Cabecera actividad",
+    )
+
+    def __str__(self):
+        return f"{self.id}"
+
+    class Meta:
+        verbose_name = "Detalle actividad"
+        verbose_name_plural = "Detalle actividades"
--- curso/models.py
+++ curso/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from persona.models import Funcionario
+
 
 # Create your models here.
 class Paralelo(models.Model):
@@ -12,3 +14,88 @@ class Paralelo(models.Model):
     class Meta:
         verbose_name = "Paralelo"
         verbose_name_plural = "Paralelos"
+
+
+NIVEL = (
+    ("1", "Educacion basica general"),
+    ("2", "Bachiller"),
+)
+
+
+class Curso(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Id")
+    nombre = models.CharField(max_length=100, verbose_name="Nombre")
+    nivel = models.CharField(max_length=5, choices=NIVEL, verbose_name="Nivel")
+    paralelo = models.ForeignKey(
+        Paralelo,
+        related_name="paralelo_curso",
+        on_delete=models.CASCADE,
+        verbose_name="Paralelo",
+    )
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+
+
+class Area(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Id")
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Area"
+        verbose_name_plural = "Areas"
+
+
+class Materia(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Id")
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    area = models.ForeignKey(
+        Area, on_delete=models.CASCADE, related_name="area_materia", verbose_name="Area"
+    )
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Materia"
+        verbose_name_plural = "Materias"
+
+
+class MateriaCursoDocente(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Id")
+    periodo = models.IntegerField(verbose_name="Periodo")
+    periodo_abierto = models.BooleanField(verbose_name="Periodo abierto")
+    inicio_periodo = models.DateField(verbose_name="Inicio periodo")
+    cierre_periodo = models.DateField(verbose_name="Cierre periodo")
+    curso = models.ForeignKey(
+        Curso,
+        related_name="curso_materia",
+        on_delete=models.CASCADE,
+        verbose_name="Curso",
+    )
+    materia = models.ForeignKey(
+        Materia,
+        related_name="materia",
+        on_delete=models.CASCADE,
+        verbose_name="Materia",
+    )
+    docente = models.ForeignKey(
+        Funcionario,
+        related_name="docente",
+        on_delete=models.CASCADE,
+        verbose_name="Funcionario",
+    )
+
+    def __str__(self):
+        return f"{self.periodo}"
+
+    class Meta:
+        verbose_name = "Materia curso docente"
+        verbose_name_plural = "Materias curso docente"
